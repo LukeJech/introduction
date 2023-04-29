8f1d175be10ef920d726849a5783472c68a2a59c
LukeJech <LukeJech@gmail.com>
2023-04-28 21:17:13 -0700

    hello world assignment added

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+name = "Luke"
+print("Hello", name)
+print("Hello " + name)
+fav_num = 9
+print ("Hello", str(fav_num))
+print ("Hello " + str(fav_num))
+
+fav_food = "Tacos"
+fav_food2 = "Crème Brûlée"
+print("I love to eat {} and {}.".format(fav_food, fav_food2))
+print(f"I love to eat {fav_food} and {fav_food2}")
